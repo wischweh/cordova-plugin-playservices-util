acea4ccac96a4f0ea3b389a040806abc2269f8e3
Jan D.S. Wischweh <j.wischweh@ndr.de>
2017-09-28 15:59:05 +0200

    implements quering the version names for selected packages

--- src/android/de/ndr/playservices_util/PlayservicesUtilPlugin.java
+++ src/android/de/ndr/playservices_util/PlayservicesUtilPlugin.java
@@ -10,10 +10,17 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.content.pm.PackageManager;
 import android.util.Log;
 
+import com.google.android.gms.common.GoogleApiAvailability;
+
 public class PlayservicesUtilPlugin extends CordovaPlugin {
-  private static final String TAG = "PlayservicesUtilPlugin";
+
+    private static final String TAG = "PlayservicesUtilPlugin";
+
+    private static final String PACKAGE_WEBVIEW="com.google.android.webview"
+
 
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
     super.initialize(cordova, webView);
@@ -22,22 +29,32 @@ public class PlayservicesUtilPlugin extends CordovaPlugin {
   }
 
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    if(action.equals("getPlayservicesVersion")) {
-    final PluginResult result = new PluginResult(PluginResult.Status.OK, String.valueOf(this.getPlayservicesVersion()));
-    callbackContext.sendPluginResult(result);
-    } else if(action.equals("getWebviewVersion")) {
-    final PluginResult result = new PluginResult(PluginResult.Status.OK, String.valueOf(this.getWebviewVersion()));
-    callbackContext.sendPluginResult(result);
+    if(action.equals("getPlayservicesVersionName")) {
+        resolvePackageVersionName(GoogleApiAvailability.GOOGLE_PLAY_SERVICES_PACKAGE,callbackContext);
+    } else if(action.equals("getWebviewVersionName")) {
+        resolvePackageVersionName(PACKAGE_WEBVIEW,callbackContext);
+    } else {
+        final PluginResult result = new PluginResult((PluginResult.Status.INVALID_ACTION,"action unknown: "+action);
+        callbackContext.sendPluginResult(result);
     }
     return true;
   }
 
-  private int getPlayservicesVersion() {
-    return -1;
-  }
 
-  private int getWebviewVersion() {
-    return -1;
+  private void resolvePackageVersionName(final String packageName, final CallbackContext callbackContext) {
+      cordova.getThreadPool().execute(new Runnable() {
+          @Override
+          public void run() {
+              try {
+                  String versionName=cordova.getActivity().getPackageManager().getPackageInfo(packageName,0).versionName;
+                  final PluginResult result = new PluginResult(PluginResult.Status.OK, versionName);
+                  callbackContext.sendPluginResult(result);
+              } catch (PackageManager.NameNotFoundException nameNotFound) {
+                  final PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Package Name Not found: "+packageName+": "+nameNotFound);
+                  callbackContext.sendPluginResult(result);
+              }
+          }
+      });
   }
 
 }
