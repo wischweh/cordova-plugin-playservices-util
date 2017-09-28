0feea82409230a629e9b4950a9bd3e8fceb5d653
Jan D.S. Wischweh <j.wischweh@ndr.de>
2017-09-28 13:24:16 +0200

    sets plugin id

---
+++ src/android/de/ndr/playservices_util/PlayservicesUtilPlugin.java
@@ -0,0 +1,43 @@
+/**
+ */
+package de.ndr.playservices_util;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.util.Log;
+
+public class PlayservicesUtilPlugin extends CordovaPlugin {
+  private static final String TAG = "PlayservicesUtilPlugin";
+
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+
+    Log.d(TAG, "Initializing PlayservicesUtilPlugin");
+  }
+
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    if(action.equals("getPlayservicesVersion")) {
+    final PluginResult result = new PluginResult(PluginResult.Status.OK, String.valueOf(this.getPlayservicesVersion()));
+    callbackContext.sendPluginResult(result);
+    } else if(action.equals("getWebviewVersion")) {
+    final PluginResult result = new PluginResult(PluginResult.Status.OK, String.valueOf(this.getWebviewVersion()));
+    callbackContext.sendPluginResult(result);
+    }
+    return true;
+  }
+
+  private int getPlayservicesVersion() {
+    return -1;
+  }
+
+  private int getWebviewVersion() {
+    return -1;
+  }
+
+}
